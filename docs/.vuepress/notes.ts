cbd396f2aacdd7dbc9988d94b855f0dbca844d62
cat0825 <1759138827@qq.com>
2025-05-20 21:30:52 +0800

    更新笔记配置以匹配实际目录结构

--- docs/.vuepress/notes.ts
+++ docs/.vuepress/notes.ts
@@ -1,13 +1,50 @@
 import { defineNoteConfig, defineNotesConfig } from 'vuepress-theme-plume'
 
-const demoNote = defineNoteConfig({
-  dir: 'demo',
-  link: '/demo',
-  sidebar: ['', 'foo', 'bar'],
+const llmNote = defineNoteConfig({
+  dir: '大语言模型学习',
+  link: '/大语言模型学习',
+})
+
+const mlNote = defineNoteConfig({
+  dir: '机器学习',
+  link: '/机器学习',
+})
+
+const cppNote = defineNoteConfig({
+  dir: 'c++-primer-plus',
+  link: '/c++-primer-plus',
+})
+
+const leetcodeNote = defineNoteConfig({
+  dir: 'leetcode',
+  link: '/leetcode',
+})
+
+const thesisNote = defineNoteConfig({
+  dir: '毕业设计',
+  link: '/毕业设计',
+})
+
+const thinoNote = defineNoteConfig({
+  dir: 'thino',
+  link: '/thino',
+})
+
+const transformerNote = defineNoteConfig({
+  dir: 'transformer',
+  link: '/transformer',
 })
 
 export const notes = defineNotesConfig({
   dir: 'notes',
   link: '/',
-  notes: [demoNote],
+  notes: [
+    llmNote,
+    mlNote,
+    cppNote,
+    leetcodeNote,
+    thesisNote,
+    thinoNote,
+    transformerNote
+  ],
 })
